b2c95447dbe644aa59dc0c5d3273bd7ab80cdcd9
Bourqu1n <noam.bourqui@studentfr.ch>
2025-03-31 11:06:16 +0200

    ApiRest1

---
+++ projet/serviceRest2/app/src/main/java/apiadmin/app/ctrl/Controller.java
@@ -0,0 +1,14 @@
+package apiadmin.app.ctrl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/admin")
+public class Controller {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> visites() {
+        return ResponseEntity.ok("Hello World");
+    }
+    
+}
