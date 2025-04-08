68580a968be03b7e3d352dfd7f37c48dd5fd0c74
GoobyWoobies <albatorix28@gmail.com>
2025-04-08 11:05:39 +0200

    Update cors error main

--- projet/apiGateway/app/src/main/java/apigateway/app/config/CorsConfig.java
+++ projet/apiGateway/app/src/main/java/apigateway/app/config/CorsConfig.java
@@ -1,16 +1,10 @@
 package apigateway.app.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
 public class CorsConfig {
-    public static void main(String[] args) {
-        SpringApplication.run(CorsConfig.class, args);
-    }
-    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
